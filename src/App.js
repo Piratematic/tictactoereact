431c43180030ed5d6c3c6dbb72ddf1ef14fe453a
Scott (DC)  Christensen <schristensen10@gmail.com>
2019-09-13 17:41:57 -0600

    Everything but the Game Logic

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch,  } from 'react-router-dom';
+import Home from "./components/Home";
+import Game from "./components/Game";
+import NoMatch from "./components/NoMatch";
+import Nav from "./components/Nav";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <>
+  <Nav />
+    <Switch>
+      <Route exact path= "/" component={Home} />
+      <Route exact path="/game"  component={Game} />
+      <Route component={NoMatch}/>
+    </Switch>
+  </>
+);
 
 export default App;
